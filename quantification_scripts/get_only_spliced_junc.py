68f5560e5b4f9130dcf28b9b957a040f16f47ae7
Kirsten Reimer <kirsten.reimer@yale.edu>
2020-01-28 15:56:53 -0500

    Create get_only_spliced_junc.py

---
+++ quantification_scripts/get_only_spliced_junc.py
@@ -0,0 +1,36 @@
+'''
+get_only_spliced_junc.py
+Author: Kirsten Reimer
+This script filters only spliced reads from all intron-spanning reads.
+Input file should be a BED12 file that has been intersected with a BED6 file of intron coordinates +/- 1 nt using bedtools intersect, e.g. "bedtools intersect -b introns_plus_1.bed -a data.bed -split -F 1 > junction_spanning_reads.bed"
+'''
+
+import os
+import sys
+import re
+
+file = sys.argv[1]
+with open(file, 'r') as bed_file:
+
+  for line in bed_file:
+      col = line.rstrip().split("\t")
+      int_coord1 = col[1]
+      int_coord2 = col[2]
+      intron = str(int(int_coord1)+1) + str(int(int_coord2)-1) # create a string of the intron coordinates
+
+      readStart = col[6]
+      readEnd = col[7]
+      blockStarts = col[11].split(",")
+      blockSizes = col[10].split(",")
+
+      n = int(col[9]) # get number of blocks to use as counter
+      junclist = [] # create an empty list for coordinates of junctions in each read
+
+      for i in range(1,n):
+          juncStart = int(readStart) + int(blockSizes[i-1]) + int(blockStarts[i-1])
+          juncEnd = int(readStart) + int(blockStarts[i])
+          junc = str(juncStart) + str(juncEnd)
+          junclist.append(junc) # add each junction to a list of junctions in the read
+
+      if intron in junclist: # look for exact intron coordinates in list of junctions in the read
+          print(line.rstrip() + "\n")
